bc6e624948e71112e6f95ecd9aec76f2bb3d324a
Your Name <Your email>
2018-01-09 13:14:25 -0700

    Created HelperModel Files

---
+++ HelperModels/RegisterUserModels.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace sharpGregsList.HelperModels
+
+{
+    public class RegisterUserModel
+    {
+        [MaxLength(20)]
+        public string Username { get; set; }
+        [MaxLength(255), EmailAddress]
+        public string Email { get; set; }
+        [MinLength(4)]
+        public string Password { get; set; }
+
+    }
+}

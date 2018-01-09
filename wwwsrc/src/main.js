34a6454efd481c459e1adafd34cbeca2c0a20ce0
Sean MacKenzie <sean.andrew.mackenzie@gmail.com>
2018-01-09 15:14:11 -0700

    file organization

--- wwwsrc/src/main.js
+++ wwwsrc/src/main.js
@@ -3,6 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+
+var server = axios.create({
+  baseURL: window.location.host.includes("localhost") ? '//localhost:5000' : '/',
+  timeout: 2000,
+  withCredentials: true
+})
+
+server.get('account/authenticate').then(x => console.log(x))
+
+server.get('api/values').then(res => {
+  console.log(res)
+}).catch(err => console.log(err))
+
 
 Vue.config.productionTip = false
 
